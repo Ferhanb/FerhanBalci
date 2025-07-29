4e3eaba6817f36db93d9e0fefba4d74b9e4c1f8d
Ferhanb <balci.ferhan@outlook.com>
2025-07-29 11:19:48 +0000

    Dag 2 Opdrachten 14, 15, 16

--- main2.py
+++ main2.py
@@ -1,19 +1,19 @@
-# # Opdracht 14
+# Opdracht 14
 
-# # In deze code zie je dat ik if, elif en else statements kan gebruiken.
+# In deze code zie je dat ik if, elif en else statements kan gebruiken.
 
-# getal = 2
+getal = 2
 
-# if getal > 0:
-#   print("Dit getal is positief.")
+if getal > 0:
+  print("Dit getal is positief.")
+elif getal < 0;
+  print("Dit getal is kleiner dan 0.")
+elif getal == 0:
+  print("Dit getal is gelijk aan 0.")
+else:
+  print("Dit is misschien geen getal?")
 
-# elif getal == 0:
-#   print("Dit getal is gelijk aan 0.")
-
-# else:
-#   print("Dit getal is kleiner dan 0.")
-
-# # Opdracht 15
+# Opdracht 15
 
 # # Vraag gebruiker waar hij/zij vandaan komt
 # land = input("Where do you come from? ") .strip().capitalize()
@@ -34,27 +34,27 @@
 # # In dit voorbeeld zie je dat het antwoord hoofdlettergevoelig is. En als de gebruiker hetzelfde land in een andere taal invoert, herkent het programma dit woord niet.
 
 
-# Opdracht 16
-# Vraag de gebruiker welke operation hij wil uitvoeren
-vraag = input("Wat wil je doen? ")
-# Vraag twee getallen
+# # Opdracht 16
+# # Vraag de gebruiker welke operation hij wil uitvoeren
+# vraag = input("Wat wil je doen? ")
+# # Vraag twee getallen
 
-x = int(input("Kies eerste getal: "))
-y = int(input("Kies tweede getal: "))
+# x = int(input("Kies eerste getal: "))
+# y = int(input("Kies tweede getal: "))
 
-# Doe de berekening
-if "Optellen" or "+" or "plus" in vraag:
-  z = x + y
-elif "Aftrekken" or "min" or "-" in vraag:
-  z = x - y
-elif "Vermenigvuldigen" or "keer" or "*" in vraag:
-  z = x * y
-elif "Delen" or ":" or "/" in vraag:
-  z = x / y
-else: 
-  print("Verkeerd")
+# # Doe de berekening
+# if "Optellen" in vraag or "+" in vraag or "plus" in vraag:
+#   z = x + y
+# elif "Aftrekken" in vraag or "min" in vraag or "-" in vraag:
+#   z = x - y
+# elif "Vermenigvuldigen" in vraag or "keer" in vraag or "*" in vraag:
+#   z = x * y
+# elif "Delen" in vraag or ":" in vraag or "/" in vraag:
+#   z = x / y
+# else: 
+#   print("Verkeerd")
 
-# Print het antwoord in een zin.
-print(f"Het antwoord van de berekening is: {z}")
+# # Print het antwoord in een zin.
+# print(f"Het antwoord van de berekening is: {z}")
 
 
